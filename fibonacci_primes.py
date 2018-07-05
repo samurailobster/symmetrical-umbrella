7dc6f8141cedb0af47206dbc64092a9a974f36b7
samurailobster <jeffreymccharen@gmail.com>
2018-07-05 06:39:44 -0500

    Refactored primality test

--- fibonacci_primes.py
+++ fibonacci_primes.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python 
 
 from functools import lru_cache
+import math
 
-@lru_cache(maxsize = 1000)
+@lru_cache(maxsize = None)
 def fibonacci(n):
     if n == 1:
         return 1
     elif n == 2:
         return 1
     elif n > 2:
-            return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n-1) + fibonacci(n-2)
 
-def is_prime_v1(n):
+def is_prime_v3(n):
     if n == 1:
-        return False
-    for d in range(2, n):
+        return
+    if n == 2:
+        print(str(n) + " is a Prime Fibonacci number.")
+    elif n > 2 and n % 2 == 0:
+        return
+    max_divisor = math.floor(math.sqrt(n))
+    for d in range(3, 1 + max_divisor, 2):
         if n % d == 0:
-            return False
-    return True
+            return
+    print(str(n) + " is a Prime Fibonacci number.")
 
-for f in range(1, 101):
-    if is_prime_v1(fibonacci(f)):
-        print(str(f) + " is a Prime Fibonacci number.")
-    else:
-        print(str(f) + " is not Prime Fibonacci number.")
+for n in range(1,25):
+    is_prime_v3(fibonacci(n))
