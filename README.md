04f45d6d4ee1fd19ddf99a942e166bef489c5556
samurailobster <jeffreymccharen@gmail.com>
2018-07-05 06:59:28 -0500

    Using Rabin-Miller algorithm to determine primality.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # symmetrical-umbrella
-Learning Python3 by creating a Fibonacci prime number generator.
+Learning Python3 by creating a Fibonacci prime number generator. Nothing special and not terribly efficient.
+
+Testing primality with the Rabin-Miller algorithm. https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
--- fibonacci_primes.py
+++ fibonacci_primes.py
@@ -13,17 +13,27 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 def is_prime_v3(n):
-    if n == 1:
-        return
-    if n == 2:
-        print(str(n) + " is a Prime Fibonacci number.")
-    elif n > 2 and n % 2 == 0:
-        return
-    max_divisor = math.floor(math.sqrt(n))
-    for d in range(3, 1 + max_divisor, 2):
-        if n % d == 0:
-            return
-    print(str(n) + " is a Prime Fibonacci number.")
+    # Returns True if num is a prime number.
+    s = n - 1
+    t = 0
+    while s % 2 == 0:
+        # keep halving s while it is even (and use t
+        # to count how many times we halve s)
+        s = s // 2
+        t += 1
+
+    for trials in range(10): # try to falsify num's primality 5 times
+        a = random.randrange(2, n - 1)
+        v = pow(a, s, n)
+        if v != 1: # this test does not apply if v is 1.
+            i = 0
+            while v != (n - 1):
+                if i == t - 1:
+                    return False
+                else:
+                    i = i + 1
+                    v = (v ** 2) % n
+                    print(str(n + " is a Prime Fibonacci number."))
 
 for n in range(1,25):
     is_prime_v3(fibonacci(n))
